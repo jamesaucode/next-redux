152d084766e74c8c194a328514393e09129cd59e
jamesaucode <aucbjames@gmail.com>
2019-09-16 02:21:03 -0700

    src components folder added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,33 +1,8 @@
-import React, { useState } from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
+import TodoList from '../src/components/TodoList';
 
-const Index = props => {
-  const [task, setTask] = useState('');
-  const handleChange = event => {
-    setTask(event.target.value);
-  };
-  const handleClick = event => {
-    event.preventDefault();
-    props.dispatch({ type: 'ADD_ITEM', payload: task });
-    setTask('');
-  };
-  return (
-    <div>
-      <h1>Here's the todos</h1>
-      <ul>
-        {props.todos.map((todo, index) => (
-          <li>{`${index}. ${todo}`}</li>
-        ))}
-      </ul>
-      <input type="text" value={task} onChange={handleChange} />
-      <button onClick={handleClick}>Add</button>
-    </div>
-  );
+const Index = () => {
+  return <TodoList />;
 };
 
-Index.getInitialProps = async ({ store, isServer, pathname, query }) => {
-  store.dispatch({ type: 'ADD_ITEM', payload: 'LMAOOOO' });
-  return {};
-};
-
-export default connect(state => state)(Index);
+export default Index;
